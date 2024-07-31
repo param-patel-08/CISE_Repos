a16620b776ab358cfd392bb84b6d6a24cea7b803
Param Patel <param.pat01@gmail.com>
2024-08-01 10:34:30 +1200

    started worksheet 3_backend

--- cise_repos/worksheet2/my-react-app/src/App.test.tsx
+++ cise_repos/worksheet2/my-react-app/src/App.test.tsx
@@ -10,7 +10,7 @@ it('renders without crashing', () => {
 
 describe('Addition', () => {
   it('knows that 2 and 2 make 4', () => {
-    expect(2 + 2).toBe(4);
+    expect(2 + 2).toBe(5);
   });
 });
 
