fb9d86158620764621667199ab797ebbb58086e8
Param Patel <param.pat01@gmail.com>
2024-07-31 12:14:52 +1200

    added the .env file changed the added the API call in the app controller file

--- cise_repos/worksheet2/my-nest-app/src/main.ts
+++ cise_repos/worksheet2/my-nest-app/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { dot } from 'node:test/reporters';
+import * as dotenv from 'dotenv'; // Import dotenv module
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  dotenv.config();
+  const PORT = process.env.PORT || 5000;
+  await app.listen(PORT);
 }
 bootstrap();
