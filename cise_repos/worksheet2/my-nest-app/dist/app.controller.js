2bcd47cc49e9366eb76dc4464dc56965fa2b57d1
Param Patel <param.pat01@gmail.com>
2024-08-17 20:26:21 +1200

    feat: Add endpoint to get articles by ID in AppController

--- cise_repos/worksheet2/my-nest-app/dist/app.controller.js
+++ cise_repos/worksheet2/my-nest-app/dist/app.controller.js
@@ -8,6 +8,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppController = void 0;
 const common_1 = require("@nestjs/common");
@@ -21,7 +24,12 @@ let AppController = class AppController {
         return this.appService.getHello();
     }
     getArticles() {
+        console.log('getArticles endpoint hit');
         return articles_1.ARTICLES;
+    }
+    getArticlesById(id) {
+        console.log('getArticlesById endpoint hit with id:', id);
+        return articles_1.ARTICLES.find(article => article._id === id);
     }
 };
 exports.AppController = AppController;
@@ -37,6 +45,13 @@ __decorate([
     __metadata("design:paramtypes", []),
     __metadata("design:returntype", Array)
 ], AppController.prototype, "getArticles", null);
+__decorate([
+    (0, common_1.Get)('/api/articles/:id'),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", Object)
+], AppController.prototype, "getArticlesById", null);
 exports.AppController = AppController = __decorate([
     (0, common_1.Controller)(),
     __metadata("design:paramtypes", [app_service_1.AppService])
