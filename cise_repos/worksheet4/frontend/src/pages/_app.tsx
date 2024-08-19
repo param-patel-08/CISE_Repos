f4fbefb94391bc6aecc3f7d3564da9a6859f3cb1
Param Patel <param.pat01@gmail.com>
2024-08-19 21:38:16 +1200

    made sure code is clean

--- cise_repos/worksheet4/frontend/src/pages/_app.tsx
+++ cise_repos/worksheet4/frontend/src/pages/_app.tsx
@@ -4,10 +4,10 @@ import { SessionProvider } from "next-auth/react";
 import PopulatedNavBar from "../components/PopulatedNavBar";
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 return (
-<SessionProvider session={session}>
-<PopulatedNavBar />
-<Component {...pageProps} />
-</SessionProvider>
+    <SessionProvider session={session}>
+        <PopulatedNavBar />
+        <Component {...pageProps} />
+    </SessionProvider>
 );
 }
 export default MyApp;
--- cise_repos/worksheet4/frontend/src/pages/articles/index.tsx
+++ cise_repos/worksheet4/frontend/src/pages/articles/index.tsx
@@ -12,42 +12,46 @@ interface ArticlesInterface {
     evidence: string;
 }
 type ArticlesProps = {
-articles: ArticlesInterface[];
+    articles: ArticlesInterface[];
 };
 const Articles: NextPage<ArticlesProps> = ({ articles }) => {
 const headers: { key: keyof ArticlesInterface; label: string }[] = [
-{ key: "title", label: "Title" },
-{ key: "authors", label: "Authors" },
-{ key: "source", label: "Source" },
-{ key: "pubyear", label: "Publication Year" },
-{ key: "doi", label: "DOI" },
-{ key: "claim", label: "Claim" },
-{ key: "evidence", label: "Evidence" },
+    { key: "title", label: "Title" },
+    { key: "authors", label: "Authors" },
+    { key: "source", label: "Source" },
+    { key: "pubyear", label: "Publication Year" },
+    { key: "doi", label: "DOI" },
+    { key: "claim", label: "Claim" },
+    { key: "evidence", label: "Evidence" },
 ];
+
 return (
-<div className="container">
-<h1>Articles Index Page</h1>
-<p>Page containing a table of articles:</p>
-<SortableTable headers={headers} data={articles} />
-</div>
+    <div className="container">
+        <h1>Articles Index Page</h1>
+        <p>Page containing a table of articles:</p>
+        <SortableTable headers={headers} data={articles} />
+    </div>
 );
 };
+
 export const getStaticProps: GetStaticProps<ArticlesProps> = async (_) => {
 // Map the data to ensure all articles have consistent property names
-const articles = data.map((article) => ({
-id: article.id ?? article._id,
-title: article.title,
-authors: article.authors,
-source: article.source,
-pubyear: article.pubyear,
-doi: article.doi,
-claim: article.claim,
-evidence: article.evidence,
+    const articles = data.map((article) => ({
+        id: article.id ?? article._id,
+        title: article.title,
+        authors: article.authors,
+        source: article.source,
+        pubyear: article.pubyear,
+        doi: article.doi,
+        claim: article.claim,
+        evidence: article.evidence,
 }));
+
 return {
-props: {
-articles,
-},
+    props: {
+        articles,
+    },
 };
 };
+
 export default Articles;
